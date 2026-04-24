4f6946c2c98d56a3cdead398599f40bf03cf620a
sharmila200429 <sharmilarose2004@gmail.com>
2026-04-24 23:12:40 +0530

    Add files via upload

---
+++ openaichatbot.py
@@ -0,0 +1,23 @@
+from openai import OpenAI
+client = OpenAI(api_key="key")
+def start_chatbot():
+    messages = [{"role":"system","content":"you are a helper and assistant"}]
+
+    print("chargebee chatbot,Type 'quit' to exit")
+
+    while True:
+        user_input=input("you:")
+        if user_input.lower() in ["quit","exit"]:
+           break
+        messages.append({"role":"user","content":user_input})
+        response = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=messages,
+            temperature=0.7
+        )
+        assistent_reply=response.choices[0].message.content
+        print(f"Idealabs:{assistant_reply}")
+        messages.append({"role","assistant","content": assistant_reply})
+
+if __name__=="__main__":
+    start_chatbot()
